ce00003ae06dd2d24367bf6eb64a76d598802c97
muhammadisa <msawjk@gmail.com>
2023-09-25 02:27:51 +0700

    Fixing

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ class ServiceFoo(services.Services):
         response_model = ResponseBase()
         response_model.response_code = "00"
         response_model.response_message = "Success"
-        # raise ServicesLevelLogicsException("logics", 500)
+        raise ServicesLevelLogicsException("logics", 500)
         # print("foo service executed")
-        return response_model, 200
+        # return response_model, 200
 
     def retrieve(self) -> (BaseModel, int):
         response_model, response_htt_code = self._logics()
