b835c1c71086793d3b7cc59f94965a58d67455ed
muhammadisa <msawjk@gmail.com>
2023-09-25 21:32:15 +0700

    Fixing

--- structure/application.py
+++
@@ -1,6 +0,0 @@
-class ApplicationService:
-    """
-    "ApplicationService" this represents first entry of the service
-    in this class where the merging, initialization, and configs
-    are provided and will be distributed between other abstraction.
-    """
--- structure/group.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-
-class Group(Blueprint):
-    def __init__(self, import_name: str, group_name: str, url_prefix: str):
-        super().__init__(group_name, import_name, url_prefix=url_prefix)
